855fe0e43d6bf90b4907318775fa0e663afeaf07
yzh168yzh168 <32607489+yzh168yzh168@users.noreply.github.com>
2017-10-08 17:48:39 +0800

    Update HelloNationalDay.py

--- 1-score-query/1704yzh/HelloNationalDay.py
+++ 1-score-query/1704yzh/HelloNationalDay.py
@@ -1,2 +1,9 @@
+
 print("Hello World")
-print("Emmm i can't write another project")
+#print("Emmm i can't write another project")
+x=[1,2,3,4,5]
+y=(99,99,98,97,96)
+for i in x:
+  print(y[i-1])
+ 
+
